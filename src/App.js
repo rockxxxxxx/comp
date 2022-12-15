8ae94760bdc9ef9a6790f7baac04e38486725178
rockxxxxxx <78143717+rockxxxxxx@users.noreply.github.com>
2022-12-15 23:52:37 +0530

    create nput fields

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const handleRegistration = (data) => console.log(data);
+  const handleError = (errors) => {};
+
+  const registerOptions = {
+    fname: {
+      required: "First Name is required",
+      minLength: {
+        value: 3,
+        message: "First name must be al tleast 3 character",
+      },
+    },
+    lname: {
+      required: "Last Name is required",
+      minLength: {
+        value: 3,
+        message: "Last name must be al tleast 3 character",
+      },
+    },
+    email: { required: "Email is required" },
+    phn: {
+      required: "Phone number is required",
+      minLength: {
+        value: 10,
+        message: "Phone number must have at least 8 characters",
+      },
+    },
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Linuxbean CRUD Test</h1>
+      <hr />
+      <form onSubmit={handleSubmit(handleRegistration, handleError)}>
+        <div className="form">
+          <div className="formt">
+            <input
+              name="fname"
+              type="text"
+              placeholder="First Name"
+              {...register("fname", registerOptions.fname)}
+            />
+            <small className="text-danger">
+              {errors?.fname && errors.fname.message}
+            </small>
+          </div>
+          <div className="formt">
+            <input
+              name="lname"
+              type="text"
+              placeholder="Last Name"
+              {...register("lname", registerOptions.lname)}
+            />
+            <small className="text-danger">
+              {errors?.lname && errors.lname.message}
+            </small>
+          </div>
+          <div className="formt">
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              {...register("email", registerOptions.email)}
+            />
+            <small className="text-danger">
+              {errors?.email && errors.email.message}
+            </small>
+          </div>
+
+          <div className="formt">
+            <input
+              type="number"
+              name="phn"
+              placeholder="Phone number"
+              {...register("phn", registerOptions.phn)}
+            />
+            <small className="text-danger">
+              {errors?.phn && errors.phn.message}
+            </small>
+          </div>
+          <button>Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
